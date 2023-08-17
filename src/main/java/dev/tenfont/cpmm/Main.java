a3a3f5873f46d9b44e6905b8e5aaaa4f102b95f8
TenFont <thetenfont@gmail.com>
2023-08-17 15:30:41 +0500

    Functionality for executing files

--- src/main/java/dev/tenfont/cpmm/Main.java
+++ src/main/java/dev/tenfont/cpmm/Main.java
@@ -1,7 +1,33 @@
 package dev.tenfont.cpmm;
 
+import dev.tenfont.cpmm.lang.Interpreter;
+import dev.tenfont.cpmm.lang.Parser;
+import dev.tenfont.cpmm.lang.components.Statement;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        // todo: everything
+        if (args.length == 0) {
+            System.err.println("Please provide a file to run as the first argument.");
+            System.exit(-1);
+        }
+        File file = new File(args[0]);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            Parser parser = new Parser(reader.lines().collect(Collectors.joining("\n")));
+            LinkedList<Statement> statements = parser.parse();
+//            System.out.println(statements);
+            Interpreter interpreter = new Interpreter(statements);
+            interpreter.execute();
+        } catch (FileNotFoundException e) {
+            System.err.println("The file specified does not exist.");
+            System.exit(-1);
+        }
     }
 }
