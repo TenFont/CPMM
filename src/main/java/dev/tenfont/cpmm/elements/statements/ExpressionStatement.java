0496f8b40e88684557161ad6ea9b15cff1cd6f1f
TenFont <thetenfont@gmail.com>
2023-09-02 15:09:39 +0500

    Change expression statement print behaviour to only print if the expression begins with a string literal

--- src/main/java/dev/tenfont/cpmm/elements/statements/ExpressionStatement.java
+++ src/main/java/dev/tenfont/cpmm/elements/statements/ExpressionStatement.java
@@ -4,20 +4,23 @@ import dev.tenfont.cpmm.lang.Parser;
 import dev.tenfont.cpmm.lang.components.Context;
 import dev.tenfont.cpmm.lang.components.Expression;
 import dev.tenfont.cpmm.lang.components.Statement;
+import dev.tenfont.cpmm.lang.components.TokenType;
 
 public class ExpressionStatement extends Statement {
+    private boolean print;
     private Expression<?> expression;
 
     @Override
     public void execute(Context context) {
         var value = expression.get(context);
-        if (value instanceof String) {
+        if (print) {
             System.out.println(value);
         }
     }
 
     @Override
     public boolean init(Parser parser, Context context) {
+        print = parser.getLexer().getLookAhead().isType(TokenType.STRING);
         expression = parser.parseExpression(context, Object.class);
         return true;
     }
